6aa846eb1626abcafa6ca54576af7cf3ac4fad11
naoyasugiyama <lynx07talisman@gmial.com>
2019-10-17 14:47:52 +0900

    django chat redisインストール前

---
+++ mysite/routing.py
@@ -0,0 +1,13 @@
+# mysite/ruting.py
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+application = ProtocolTypeRouter({
+    # (http->django view is added by default)
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
